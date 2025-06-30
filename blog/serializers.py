1db67f972d01d6a52d8ffe22baee19c86a7b65e9
Christian Vuye <christianvuye@Christians-MacBook-Pro.local>
2025-06-30 22:01:23 +0200

    Add validation and creation logic to PostSerializer
    - Add title length validation (minimum 5 characters)
    - Handle many-to-many category relationships in create method

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,10 +1,29 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from .models import Post, Category
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'title', 'body', 'created_on', 'last_modified', 'categories', 'page_type']
+    
+    def validate(self, data):
+        title = data["title"]
+
+        if len(title) < 5:
+            raise ValidationError("The title should be at least five characters.")
+        
+        return data
+    
+    def create(self, validated_data):
+        categories = validated_data.pop("categories")
+
+        post = Post(**validated_data)
+        post.save()
+        
+        post.categories.set(categories)
+        
+        return post
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
