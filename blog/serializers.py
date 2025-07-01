bf5b451c297949ff8ce8e396db6905eddc922078
Christian Vuye <christianvuye@Christians-MacBook-Pro.local>
2025-07-01 02:31:05 +0200

    Improve PostSerializer with nested category representation
    - Add nested CategorySerializer to PostSerializer for detailed category data
    - Update create method to handle nested category objects
    - Reorganize serializer class order

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Post, Category
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'slug']
+
 class PostSerializer(serializers.ModelSerializer):
+    categories = CategorySerializer(many=True)
+    
     class Meta:
         model = Post
         fields = ['id', 'title', 'body', 'created_on', 'last_modified', 'categories', 'page_type']
@@ -9,14 +16,11 @@ class PostSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         categories = validated_data.pop("categories")
 
+        categories_ids = [category["id"] for category in categories]
+
         post = Post(**validated_data)
         post.save()
         
-        post.categories.set(categories)
+        post.categories.set(categories_ids)
         
         return post
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['id', 'name', 'slug']
